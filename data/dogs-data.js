5e447df6e1041ed839d0ab0edf1af2e6dc15b572
Cazier92 <bryce.cazier@gmail.com>
2023-01-03 16:59:13 -0800

    initialize views and data

---
+++ data/dogs-data.js
@@ -0,0 +1,21 @@
+const dogs = [
+    {
+        breed: 'husky',
+        isAdorable: true,
+        _id: 123456
+    },
+    {
+        breed: 'chihuahua',
+        isAdorable: false,
+        _id: 127984
+    },
+    {
+        breed: 'klee kai',
+        isAdorable: true,
+        _id: 129345
+    },
+]
+
+export {
+    dogs
+}
--- server.js
+++ server.js
@@ -8,12 +8,20 @@ const app = express()
 
 // Configure the app (app.set)
 
+app.set('view engine', 'ejs')
 
 // Mount Middleware (app.use)
 
 
 // Mount routes
 
+app.get('/', function(req, res) {
+    res.redirect('home')
+})
+
+app.get('/home', function(req, res) {
+    res.render('home')
+})
 
 // Tell the app to listen on port 3000
 
